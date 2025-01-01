fc8fcee37c1823d47b295b87d4245c3a81b02536
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-01 23:55:51 +0100

    modal style

---
+++ src/components/add-transaction/index.jsx
@@ -0,0 +1,42 @@
+import { Button, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Radio, RadioGroup } from '@chakra-ui/react'
+import React from 'react'
+
+export default function TransactionForm() {
+    return (
+        <Modal>
+            <form>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Add new Transaction</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <FormControl>
+                            <FormLabel>Enter Description</FormLabel>
+                            <Input
+                                placeholder='Enter Transaction description'
+                                name='description'
+                                type='text'
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Enter Amount</FormLabel>
+                            <Input
+                                placeholder='Enter Transaction amount'
+                                name='amount'
+                                type='number'
+                            />
+                        </FormControl>
+                        <RadioGroup mt='5'>
+                            <Radio value='income' colorScheme='blue' name='type'>Expense</Radio>
+                            <Radio value='expense' colorScheme='red' name='type'>Income</Radio>
+                        </RadioGroup>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button mr={'4'}>Cancel</Button>
+                        <Button>Add</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </form>
+        </Modal>
+    )
+}
